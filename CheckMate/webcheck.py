54969c6c6bb63e3608568a050c839f83f6c6776c
eaydin <emraydin@gmail.com>
2019-01-03 23:21:20 +0300

    basic client for heartbeat

--- CheckMate/webcheck.py
+++ CheckMate/webcheck.py
@@ -2,6 +2,7 @@ import urllib.request
 import urllib.error
 import time
 import threading
+import json
 
 
 def curl_check(address):
@@ -33,5 +34,47 @@ def run_curl_checker(addresses, timer=10):
     print(addresses)
 
     t = threading.Thread(target=curl_checker, args=[addresses, timer])
+    t.daemon = True
+    t.start()
+
+
+def mate_check_data(address_port):
+
+    mate_address = 'http://{addr_port}/uptime'.format(addr_port=address_port)
+
+    try:
+        t = urllib.request.urlopen(mate_address)
+        return t
+    except urllib.error.HTTPError as err:
+        print('HTTP error while checking mate {mate}: {err}'.format(mate=mate_address, err=str(err)))
+        return False
+    except Exception as err:
+        print('Error while checking mate {mate}: {err}'.format(mate=mate, err=str(err)))
+        return False
+
+
+def mate_check(address_port, timer):
+
+    while True:
+        for addr_port in address_port:
+
+            data = mate_check_data(addr_port)
+            if not data:
+                pass
+            else:
+                data_html = data.read()
+                encoding = data.info().get_content_charset('utf-8')
+                json_data = json.loads(data_html.decode(encoding))
+                print('Response of heartbeat is')
+                print(json_data)
+        time.sleep(timer)
+
+
+def run_mate_check(addresses, timer):
+
+    print('Starting mate checking for addresses')
+    print(addresses)
+
+    t = threading.Thread(target=mate_check, args=[addresses, timer])
     t.daemon = True
     t.start()
--- checkmate.py
+++ checkmate.py
@@ -2,6 +2,7 @@ import signal
 import datetime
 from CheckMate.webcheck import run_curl_checker
 from CheckMate.mate import run_mate_service
+from CheckMate.webcheck import run_mate_check
 import CheckMate.mate
 import argparse
 
@@ -19,11 +20,18 @@ if __name__ == '__main__':
                         required=False)
     parser.add_argument('--webcheck-timer', help='Time to wait between each check, in seconds.', default=10,
                         required=False, type=int)
+
+    parser.add_argument('--mate', help='Mate to check, with address and port. Ex: 192.168.30.5:4398, Checkmate will '
+                                       'handle the protocol and paths. You can specify multiple mates separated '
+                                       'with spaces.', nargs='+', default=False, required=False)
+
+    parser.add_argument('--mate-timer', help='Timer to check mates.', default=5, required=False, type=int)
+
     args = parser.parse_args()
 
     start_status = False
 
-    if not args.serve_mate and not args.webcheck:
+    if not args.serve_mate and not args.webcheck and not args.mate:
         print('You should specify some arguments.')
         raise SystemExit(1)
     if args.serve_mate:
@@ -32,6 +40,9 @@ if __name__ == '__main__':
         start_status = True
     if args.webcheck:
         run_curl_checker(args.webcheck, args.webcheck_timer)
+        start_status = True
+    if args.mate:
+        run_mate_check(args.mate, args.mate_timer)
         start_status = True
 
     if start_status:
