131698d5fdc48a02269e597f99d40194f7369aa7
M. Emre Aydin <emraydin@gmail.com>
2019-01-07 20:30:35 +0300

    metavar for arguments

--- checkmate.py
+++ checkmate.py
@@ -12,29 +12,31 @@ if __name__ == '__main__':
 
     description = """Checkmate for various network connectivity by Veriteknik"""
 
-    parser = argparse.ArgumentParser(prog='checkmate.py', formatter_class=argparse.RawDescriptionHelpFormatter,
+    parser = argparse.ArgumentParser(prog='checkmate.py', formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      fromfile_prefix_chars='@', description=description)
     parser.add_argument('--serve-mate', help='Serve HTTP Server with uptime and status, Specify TCP Port',
-                        required=False, default=False, type=int)
+                        required=False, default=False, type=int, metavar="<port>")
     parser.add_argument('--web-check', help='Domain (with protocol and port) to check via urllib. You can specify '
                                             'multiple domains separated with spaces.', nargs='+', default=False,
-                        required=False)
+                        required=False, metavar="<domain_to_check>")
     parser.add_argument('--web-check-timer', help='Time to wait between each check, in seconds.', default=10,
-                        required=False, type=int)
+                        required=False, type=int, metavar="<time>")
 
     parser.add_argument('--mate', help='Mate to check, with address and port. Ex: 192.168.30.5:4398, Checkmate will '
                                        'handle the protocol and paths. You can specify multiple mates separated '
-                                       'with spaces.', nargs='+', default=False, required=False)
+                                       'with spaces.', nargs='+', default=False, required=False,
+                                        metavar="<ip:port>")
 
-    parser.add_argument('--mate-timer', help='Timer to check mates.', default=5, required=False, type=int)
+    parser.add_argument('--mate-timer', help='Timer to check mates.', default=5, required=False, type=int,
+                        metavar="<time>")
 
     parser.add_argument('--tcp-check', help='TCP Address/Ports to check. Ex: 192.168.70.40:443. You can specify '
                                             'multiple TCP Servers separated with spaces.', nargs='+', default=False,
-                                            required=False)
+                                            required=False, metavar="<ip:port>")
     parser.add_argument('--tcp-check-close', help='Time to close TCP connection (in seconds) before checking if open.',
-                        default=2, required=False, type=int)
+                        default=2, required=False, type=int, metavar="<time>")
     parser.add_argument('--tcp-check-timer', help='Time to wait between each TCP check in seconds.', default=10,
-                        required=False, type=int)
+                        required=False, type=int, metavar="<time>")
 
     args = parser.parse_args()
 
